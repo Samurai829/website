aa5afedaa34d1d2126362536a1ab5b5143224db5
samurai829 <stevengarcia.94@hotmail.com>
2024-06-13 20:22:48 -0400

    update

--- samurai829_files/js/404.js
+++ samurai829_files/js/404.js
@@ -1,77 +1,87 @@
-function drawVisor() {
-    const canvas = document.getElementById('visor');
-    const ctx = canvas.getContext('2d');
-    
-    ctx.beginPath();
-    ctx.moveTo(5, 45);
-    ctx.bezierCurveTo(15, 64, 45, 64, 55, 45);
-    
-    ctx.lineTo(55, 20);
-    ctx.bezierCurveTo(55, 15, 50, 10, 45, 10);
-    
-    ctx.lineTo(15, 10);
-    
-    ctx.bezierCurveTo(15, 10, 5, 10, 5, 20);
-    ctx.lineTo(5, 45);
-    
-    ctx.fillStyle = '#2f3640';
-    ctx.strokeStyle = '#f5f6fa';
-    ctx.fill();
-    ctx.stroke();
-  }
-  
-  const cordCanvas = document.getElementById('cord');
-  const ctx = cordCanvas.getContext('2d');
-  
-  let y1 = 160;
-  let y2 = 100;
-  let y3 = 100;
-  
-  let y1Forward = true;
-  let y2Forward = false;
-  let y3Forward = true;
-  
-  function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    
-    ctx.beginPath();
-    ctx.moveTo(130, 170);
-    ctx.bezierCurveTo(250, y1, 345, y2, 400, y3);
-    
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 8;
-    ctx.stroke();
-   
-    
-    if (y1 === 100) {
-      y1Forward = true;
-    }
-    
-    if (y1 === 300) {
-      y1Forward = false;
-    }
-    
-    if (y2 === 100) {
-      y2Forward = true;
-    }
-    
-    if (y2 === 310) {
-      y2Forward = false;
-    }
-    
-    if (y3 === 100) {
-      y3Forward = true;
-    }
-    
-    if (y3 === 317) {
-      y3Forward = false;
-    }
-    
-    y1Forward ? y1 += 1 : y1 -= 1;
-    y2Forward ? y2 += 1 : y2 -= 1;
-    y3Forward ? y3 += 1 : y3 -= 1;
-  }
-  
-  drawVisor();
-  animate();
+gsap.set("svg", { visibility: "visible" });
+gsap.to("#headStripe", {
+  y: 0.5,
+  rotation: 1,
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut",
+  duration: 1
+});
+gsap.to("#spaceman", {
+  y: 0.5,
+  rotation: 1,
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut",
+  duration: 1
+});
+gsap.to("#craterSmall", {
+  x: -3,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut"
+});
+gsap.to("#craterBig", {
+  x: 3,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut"
+});
+gsap.to("#planet", {
+  rotation: -2,
+  yoyo: true,
+  repeat: -1,
+  duration: 1,
+  ease: "sine.inOut",
+  transformOrigin: "50% 50%"
+});
+
+gsap.to("#starsBig g", {
+  rotation: "random(-30,30)",
+  transformOrigin: "50% 50%",
+  yoyo: true,
+  repeat: -1,
+  ease: "sine.inOut"
+});
+gsap.fromTo(
+  "#starsSmall g",
+  { scale: 0, transformOrigin: "50% 50%" },
+  { scale: 1, transformOrigin: "50% 50%", yoyo: true, repeat: -1, stagger: 0.1 }
+);
+gsap.to("#circlesSmall circle", {
+  y: -4,
+  yoyo: true,
+  duration: 1,
+  ease: "sine.inOut",
+  repeat: -1
+});
+gsap.to("#circlesBig circle", {
+  y: -2,
+  yoyo: true,
+  duration: 1,
+  ease: "sine.inOut",
+  repeat: -1
+});
+
+gsap.set("#glassShine", { x: -68 });
+
+gsap.to("#glassShine", {
+  x: 80,
+  duration: 2,
+  rotation: -30,
+  ease: "expo.inOut",
+  transformOrigin: "50% 50%",
+  repeat: -1,
+  repeatDelay: 8,
+  delay: 2
+});
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click',(e) => {
+  burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed"
+  nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed"
+})
